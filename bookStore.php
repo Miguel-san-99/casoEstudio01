e8c276c955af6fb3e1bb775c991a0b8982b19c3e
Miguel-san-99 <mike.one10@hotmail.com>
2023-10-12 12:21:05 -0600

    Actualizar frame

--- bookStore.php
+++ bookStore.php
@@ -22,8 +22,18 @@ class mainFrame extends wxFrame {
 
 		$bSizerMain2 = new wxBoxSizer( wxVERTICAL );
 
+		$bSizer3 = new wxBoxSizer( wxHORIZONTAL );
+
+		$this->m_searchCtrl1 = new wxSearchCtrl( $this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, 0 );
+		$this->m_searchCtrl1->ShowSearchButton( true );
+		$this->m_searchCtrl1->ShowCancelButton( false );
+		$bSizer3->Add( $this->m_searchCtrl1, 0, wxALL|wxSHAPED, 0 );
+
+
+		$bSizerMain2->Add( $bSizer3, 0, wxALIGN_CENTER|wxALL, 0 );
+
 		$this->m_panelMain = new wxPanel( $this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL );
-		$bSizerMain2->Add( $this->m_panelMain, 1, wxEXPAND | wxALL, 0 );
+		$bSizerMain2->Add( $this->m_panelMain, 1, wxEXPAND|wxALL, 0 );
 
 
 		$bSizerMain->Add( $bSizerMain2, 1, wxALL|wxEXPAND, 0 );
